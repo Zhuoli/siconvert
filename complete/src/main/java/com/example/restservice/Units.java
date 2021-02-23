058c48f90dcd4745e33d03ceb167178bb0e69b2b
zhuoli liang <zhuoliseattle@gmail.com>
2021-02-23 15:19:12 -0800

    update json response type

--- complete/src/main/java/com/example/restservice/Units.java
+++ complete/src/main/java/com/example/restservice/Units.java
@@ -2,18 +2,12 @@ package com.example.restservice;
 
 public class Units {
 
-	private final long id;
 	private final String unitName;
 	private final double multiplicationFactor;
 
-	public Units(long id, String unitName, double multiplicationFactor) {
-		this.id = id;
+	public Units(String unitName, double multiplicationFactor) {
 		this.unitName = unitName;
 		this.multiplicationFactor=multiplicationFactor;
-	}
-
-	public long getId() {
-		return id;
 	}
 
 	public String getUnitName() {
--- complete/src/main/java/com/example/restservice/UnitsController.java
+++ complete/src/main/java/com/example/restservice/UnitsController.java
@@ -1,7 +1,5 @@
 package com.example.restservice;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UnitsController {
 
-	private static final String TEMPLATE = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-
 	@GetMapping("/units/si")
-	public Units units(@RequestParam(value = "units", defaultValue = "World") String units) {
-		return new Units(counter.incrementAndGet(), String.format(TEMPLATE, units), 0.00029088820866572);
+	public Units units(@RequestParam(value = "units", defaultValue = "(rad/s)") String units) {
+		return new Units(units, 0.00029088820866572);
 	}
 }
