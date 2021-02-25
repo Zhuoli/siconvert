136da43ec5bd8ea9c6730e53b7ed1e84e19036ac
zhuoli liang <zhuoliseattle@gmail.com>
2021-02-25 10:32:14 -0800

    update calculator

--- complete/src/main/java/com/example/restservice/SIUnitSingleton.java
+++ complete/src/main/java/com/example/restservice/SIUnitSingleton.java
@@ -31,157 +31,40 @@ public class SIUnitSingleton {
         }
         return instance;
     }
-
     public int calculate(String s) {
-
-        Stack<Integer> stack = new Stack<Integer>();
-        int operand = 0;
-        int result = 0; // For the on-going result
-        int sign = 1;  // 1 means positive, -1 means negative
+        int l2 = 1, o2 = 1;
 
         for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
+            if (Character.isDigit(c)) {
+                int num = c - '0';
 
-                // Forming operand, since it could be more than one digit
-                operand = 10 * operand + (int) (ch - '0');
-
-            } else if (ch == '+') {
-
-                // Evaluate the expression to the left,
-                // with result, sign, operand
-                result += sign * operand;
-
-                // Save the recently encountered '+' sign
-                sign = 1;
-
-                // Reset operand
-                operand = 0;
-
-            } else if (ch == '-') {
-
-                result += sign * operand;
-                sign = -1;
-                operand = 0;
-
-            } else if (ch == '(') {
-
-                // Push the result and sign on to the stack, for later
-                // We push the result first, then sign
-                stack.push(result);
-                stack.push(sign);
-
-                // Reset operand and result, as if new evaluation begins for the new sub-expression
-                sign = 1;
-                result = 0;
-
-            } else if (ch == ')') {
-
-                // Evaluate the expression to the left
-                // with result, sign and operand
-                result += sign * operand;
-
-                // ')' marks end of expression within a set of parenthesis
-                // Its result is multiplied with sign on top of stack
-                // as stack.pop() is the sign before the parenthesis
-                result *= stack.pop();
-
-                // Then add to the next operand on the top.
-                // as stack.pop() is the result calculated before this parenthesis
-                // (operand on stack) + (sign on stack * (result from parenthesis))
-                result += stack.pop();
-
-                // Reset the operand
-                operand = 0;
-            }
-        }
-        return result + (sign * operand);
-    }
-
-
-    public int calculate2(String s) {
-
-        Stack<Integer> stack = new Stack<Integer>();
-        int operand = 0;
-        int result = 1; // For the on-going result
-        int sign = 1; //  1 means * -1 means /  
-
-        for (int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
-
-                // Forming operand, since it could be more than one digit
-                operand = 10 * operand + (int) (ch - '0');
-
-            } else if (ch == '*') {
-
-                sign=1;
-                // Evaluate the expression to the left,
-                // with result, sign, operand
-                if (operand!=0){
-                    if (sign==1){
-                        result = result * operand;
-                    }else{
-                        result = result / operand;
-                    }
-                    // Reset operand
-                    operand = 0;
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    num = num * 10 + (s.charAt(++i) - '0');
                 }
 
-            } else if (ch == '/') {
-                sign=-1;
-                if (operand!=0){
-                    if (sign==1){
-                        result = result * operand;
-                    }else{
-                        result = result / operand;
-                    }
-                    operand = 0;
+                l2 = (o2 == 1 ? l2 * num : l2 / num);
+
+            } else if (c == '(') {
+                int j = i;
+
+                for (int cnt = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == '(') cnt++;
+                    if (s.charAt(i) == ')') cnt--;
+                    if (cnt == 0) break;
                 }
 
-            } else if (ch == '(') {
+                int num = calculate(s.substring(j + 1, i));
 
-                // Push the result and sign on to the stack, for later
-                // We push the result first, then sign
-                stack.push(result);
-                stack.push(sign);
+                l2 = (o2 == 1 ? l2 * num : l2 / num);
 
-                // Reset operand and result, as if new evaluation begins for the new sub-expression
-                result = 1;
+            } else if (c == '*' || c == '/') {
+                o2 = (c == '*' ? 1 : -1);
 
-            } else if (ch == ')') {
-
-                // Evaluate the expression to the left
-                // with result, sign and operand
-                if (sign==1){
-                    result = result * operand;
-                }else{
-                    result = result / operand;
-                }
-
-                // ')' marks end of expression within a set of parenthesis
-                // Its result is multiplied with sign on top of stack
-                // as stack.pop() is the sign before the parenthesis
-                sign = stack.pop();
-                if (sign==1){
-                    result = stack.pop() * result ;
-                }else{
-                    result = stack.pop() / result;
-                }
-                // Reset the operand
-                operand = 0;
-            }
+            } 
         }
-        if (operand==0){
-            operand=1;
-        }
-        if (sign==1){
-            result = result * operand;
-        }else{
-            result = result / operand;
-        }
-        return result;
+
+        return l2;
     }
 }
